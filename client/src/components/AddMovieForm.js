41ef839efe69096dc21939d23c0af6dbef3cecc3
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-16 23:13:36 -0600

    addmovieform.js set up

--- client/src/components/AddMovieForm.js
+++ client/src/components/AddMovieForm.js
@@ -37,5 +37,55 @@ const AddMovieForm = (props) => {
     }
 
     const { title, director, genre, metascore, description } = movie;
+
+    return (
+        <div>
+            <div className='modal-content'>
+                <form onSubmit={handleSubmit}>
+                    <div className='modal-header'>
+                        <h3>Editing: {movie.title}</h3>
+                    </div>
+                    <div className='modal-body'>
+                        <div className='form-input'>
+                            <lable>Title</lable>
+                            <input value={title}
+                            onChange={handleChange}
+                            name='title' type='text'/>
+                        </div>
+                        <div className='from-input'>
+                            <label>Director</label>
+                            <input value={director}
+                            onChange={handleChange}
+                            name='director' type='text'/>
+                        </div>
+                        <div className='form-input'>
+                            <lable>Genre</lable>
+                            <input value={genre}
+                            onChange={handleChange}
+                            name='genre' type='text' />
+                        </div>
+                        <div className='form-input'>
+                            <lable>Metascore</lable>
+                            <input value={metascore}
+                            onChange={handleChange}
+                            name='metascore' type='number' />
+                        </div>
+                        <div className='form-input'>
+                            <lable>Discription</lable>
+                            <input value={discription}
+                            onChange={handleChange}
+                            name='discription' type='text' />
+                        </div>
+                        <div className='modal-footer'>
+                            <input type='submit' value='Save' />
+                            <Link to={`/movies`}>
+                                <input type='button' value='Cancel' /> 
+                            </Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
